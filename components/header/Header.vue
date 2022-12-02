fecb105dc47efe88ddb9551cba0275c8ab273642
FzeroH <vampir666x@gmail.com>
2022-12-03 01:56:25 +0300

    Create Header component

---
+++ components/header/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="landing__header">
+    <img src="@/assets/images/logo.svg" alt="logo" class="landing__header-logo"/>
+    <img src="@/assets/images/search.svg" alt="search" class="landing__header-search"/>
+    <div>
+      <a href="/">Menu</a>
+    </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+  .landing__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem;
+
+    .landing__header-logo {
+      max-width: 10.9rem;
+      max-height: 4rem;
+    }
+
+    .landing__header-search {
+      max-width: 2rem;
+      max-height: 2rem;
+    }
+    div {
+      display: flex;
+      align-items: center;
+      position: relative;
+
+      a {
+        font-weight: 300;
+        font-size: 1.5rem;
+        line-height: 1.75rem;
+      }
+    }
+  }
+</style>
