3a6b4c6b629ab38c57ff58771c3562346c830d47
FzeroH <saurus.48@mail.ru>
2023-01-07 13:40:51 +0300

    Create arrow component

---
+++ components/what-i-do/Arrow.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="arrow">
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Arrow'
+}
+</script>
+
+<style lang="scss" scoped>
+  .arrow, .arrow::before, .arrow::after {
+    position: absolute;
+    right: 0;
+    background-color: #fff;
+    width: 42px;
+    &::before {
+      transform: rotate(45deg);
+    }
+    &::after {
+      transform: rotate(-45deg);
+    }
+  }
+</style>
